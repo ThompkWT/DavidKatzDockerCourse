ed0cf0fb71999e4d176c720947df4955b29f3b96
Sean Hong <iamseanhong@gmail.com>
2018-02-19 13:40:38 -0800

    Able to fetch data from postgres container by using express server

--- compose-network/clothing/server.js
+++ compose-network/clothing/server.js
@@ -7,7 +7,7 @@ const app = express();
 const pool = new Pool({ user: 'postgres', host: 'db'}); // we can just do 'db' cuz compose create DNS
 
 app.get('/', (req, res) => {
-  poll.query('SELECT * FROM apparel', (err, resFromPostgres) => {
+  pool.query('SELECT * FROM clothing', (err, resFromPostgres) => {
     res.json(resFromPostgres.rows)
   });
 });
