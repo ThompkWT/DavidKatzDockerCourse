9535513b50eb388ccda653305135861588505a29
Sean Hong <iamseanhong@gmail.com>
2018-02-19 10:50:09 -0800

    Initial commit

---
+++ node-php-volumes/players/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const HOST = '0.0.0.0';
+const PORT = '80';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    players: ['Justina', 'Carlee', 'Travis', "Aiden", "Madelynn"],
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening to ${HOST}:${PORT}`);
+});
