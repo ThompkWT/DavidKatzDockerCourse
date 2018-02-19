ee1d07b1707408673f38390268eacda7e7b9e9b7
Sean Hong <iamseanhong@gmail.com>
2018-02-19 13:18:34 -0800

    Work on express server.js to fetch data from postgresql

---
+++ compose-network/clothing/server.js
@@ -0,0 +1,19 @@
+// set up express server that will fetch data from postgresql
+const express = require('express');
+const { Pool } = require('pg');
+const PORT = 80;
+const app = express();
+
+const pool = new Pool({ user: 'postgres', host: 'db'}); // we can just do 'db' cuz compose create DNS
+
+app.get('/', (req, res) => {
+  poll.query('SELECT * FROM apparel', (err, resFromPostgres) => {
+    res.json(resFromPostgres.rows)
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listing to PORT: ${PORT}`);
+});
+
+
